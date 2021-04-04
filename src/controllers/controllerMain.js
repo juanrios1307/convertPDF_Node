d10bdeaf612d77fddef971dc031933b3d3840821
juan rios <juan.rios49@eia.edu.co>
2021-04-04 13:01:45 -0500

    send to email

--- src/controllers/controllerMain.js
+++ src/controllers/controllerMain.js
@@ -1,68 +1,100 @@
 const ControllerMain={}
 const PDFDocument = require('pdfkit');
+const nodemailer = require('nodemailer');
+
 const fs = require('fs');
 
-ControllerMain.PDF = (req,res ) =>{
+ControllerMain.PDF = async (req, res) => {
 
-    const {ciudad,nombre,representante,nit,correo,direccion,telefono,ciudadA,asunto,mensaje,secretaria,ocupacion,redactor}=req.body
+    const {ciudad, nombre, representante, nit, correo, direccion, telefono, ciudadA, asunto, mensaje, secretaria, ocupacion, redactor} = req.body
 
-    if(req.body !== null){
-    
+    if (req.body !== null) {
+
         let pdfDoc = new PDFDocument;
-        const font='Times-Roman';
 
-        pdfDoc.pipe(fs.createWriteStream('src/controllers/PDFDoc.pdf'));
-        console.log(fs.createWriteStream('PDFDoc.pdf').path)
-        pdfDoc.image('src/controllers/logo.png',350,50,{align:'right'});
-        pdfDoc.fontSize(18).font(font).text(ciudad,{align:'left'});
-        pdfDoc.fontSize(15).font(font).text(" ",{align:'left'});
+        let buffers = [];
+        pdfDoc.on('data', buffers.push.bind(buffers));
+        pdfDoc.on('end', () => {
 
-        pdfDoc.fontSize(18).font(font).text("Señor",{align:'left'});
-        pdfDoc.fontSize(20).font(font).text(nombre,{align:'left'});
-        pdfDoc.fontSize(18).font(font).text(representante,{align:'left'});
-        pdfDoc.fontSize(18).font(font).text(nit,{align:'left'});
-        pdfDoc.fontSize(18).font(font).text(correo,{align:'left'});
-        pdfDoc.fontSize(18).font(font).text(direccion,{align:'left'});
-        pdfDoc.fontSize(18).font(font).text(telefono,{align:'left'});
-        pdfDoc.fontSize(18).font(font).text(ciudadA,{align:'left'});
-        pdfDoc.fontSize(15).font(font).text(" ",{align:'left'});
+            let pdfData = Buffer.concat(buffers);
 
-        pdfDoc.fontSize(18).font(font).text("Asunto: "+asunto,{align:'left'});
-        pdfDoc.fontSize(15).font(font).text(" ",{align:'left'});
+            var transporter = nodemailer.createTransport({
+                service: 'Outlook365',
+                auth: {
+                    user: 'quickservices20202@hotmail.com',
+                    pass: '2Juan1Santiago'
+                }
+            });
 
-        pdfDoc.fontSize(15).font(font).text(mensaje,{align:'justify'});
-        pdfDoc.fontSize(45).font(font).text(" ",{align:'left'});
+            const mailOptions = {
+                from: 'quickservices20202@hotmail.com',
+                to: "juanesrios13@gmail.com",
+                attachments: [{
+                    filename: 'attachment.pdf',
+                    content: pdfData
+                }]
+            };
 
-        pdfDoc.fontSize(15).font(font).text(secretaria,{align:'left'});
-        pdfDoc.fontSize(15).font(font).text(ocupacion,{align:'left'});
-        pdfDoc.fontSize(15).font(font).text("Redactor: "+redactor,{align:'left'});
-    
-       
+            mailOptions.subject = 'PDF in mail';
+            mailOptions.text = 'PDF attached';
+
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+
+                    res.status(203).json({status: "error", data: error});
+                } else {
+                    res.status(200).json({status: "ok", data: "PDF enviado"});
+                }
+            });
+
+        });
+
+
+        const font = 'Times-Roman';
+
+
+
+        //pdfDoc.pipe(fs.createWriteStream('PDFDoc.pdf'));
+
+        pdfDoc.image('src/controllers/logo.png', 350, 50, {align: 'right'});
+        pdfDoc.fontSize(18).font(font).text(ciudad, {align: 'left'});
+        pdfDoc.fontSize(15).font(font).text(" ", {align: 'left'});
+
+        pdfDoc.fontSize(18).font(font).text("Señor", {align: 'left'});
+        pdfDoc.fontSize(20).font(font).text(nombre, {align: 'left'});
+        pdfDoc.fontSize(18).font(font).text(representante, {align: 'left'});
+        pdfDoc.fontSize(18).font(font).text(nit, {align: 'left'});
+        pdfDoc.fontSize(18).font(font).text(correo, {align: 'left'});
+        pdfDoc.fontSize(18).font(font).text(direccion, {align: 'left'});
+        pdfDoc.fontSize(18).font(font).text(telefono, {align: 'left'});
+        pdfDoc.fontSize(18).font(font).text(ciudadA, {align: 'left'});
+        pdfDoc.fontSize(15).font(font).text(" ", {align: 'left'});
+
+        pdfDoc.fontSize(18).font(font).text("Asunto: " + asunto, {align: 'left'});
+        pdfDoc.fontSize(15).font(font).text(" ", {align: 'left'});
+
+        pdfDoc.fontSize(15).font(font).text(mensaje, {align: 'justify'});
+        pdfDoc.fontSize(45).font(font).text(" ", {align: 'left'});
+
+        pdfDoc.fontSize(15).font(font).text(secretaria, {align: 'left'});
+        pdfDoc.fontSize(15).font(font).text(ocupacion, {align: 'left'});
+        pdfDoc.fontSize(15).font(font).text("Redactor: " + redactor, {align: 'left'});
+
+
         pdfDoc.end();
 
-        return  res.status(200).json({status: "ok", data: 'PDF ok'});
+
+        //return  res.status(200).json({status: "ok", data: 'PDF ok'});
 
         //return  ControllerMain.download(req,res,'PDFDoc.pdf')
 
-    }else{
+    } else {
         return (res.type('json').status(422).send({
             status: "error",
             data: "No hay DATOS"
         }));
 
     }
-}
-
-ControllerMain.download = (req,res,id ) =>{
-    //const id=req.params.id
-    res.download(__dirname+'/'+id,id,function(err){
-        if(err){
-            console.log(err)
-        }else{
-            console.log('PDF creado')
-            //res.status(200).json({status: "ok", data: "PDF creado"});
-        }
-    })
 }
 
 module.exports=ControllerMain
